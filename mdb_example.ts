0cc17eafc564d1d6b7c0eb537c5a84e161d8e3b3
calangoni <calangoni@gmail.com>
2020-09-28 15:16:08 -0300

    Update mdb_example.ts

--- mdb_example.ts
+++ mdb_example.ts
@@ -1,34 +1,30 @@
-import { Collection, Db } from 'mongodb';
+import { Collection } from 'mongodb';
 import { getDb, querySingle } from './mongoClient';
 import * as configs from '../configs.json';
 
 const mdb = {
-  db: null as Db,
-  cols: {
-    users: null as Collection<{ userId: string, pw: string }>,
-  },
-
-  async _init () {
-    mdb.db = await getDb(configs.mongodb.dbname);
-    const existingCols = (await mdb.db.collections()).map(x => x.collectionName);
-    for (const colName of Object.keys(mdb.cols)) {
-      if (!existingCols.includes(colName)) {
-        await mdb.db.createCollection(colName);
-      }
-    }
-    mdb.cols.users = mdb.db.collection('users');
-    await mdb.users._init();
-  },
-
   users: {
+    col: null as Collection<{ userId: string, pw: string }>,
     async _init () {
-      if (!(await mdb.cols.users.indexExists('unique_userId'))) {
-        await mdb.cols.users.createIndex({ userId: 1 }, { unique: true, name: 'unique_userId' })
+      if (!(await mdb.users.col.indexExists('unique_userId'))) {
+        await mdb.users.col.createIndex({ userId: 1 }, { unique: true, name: 'unique_userId' })
       }
     },
-    insert (qPars: { userId: string, pw: string }) { return mdb.cols.users.insertOne(qPars) },
-    async selectOne (qPars: { userId: string }) { return querySingle(mdb.cols.users, qPars) },
-  },
+    insert (qPars: { userId: string, pw: string }) { return mdb.users.col.insertOne(qPars) },
+    async selectOne (qPars: { userId: string }) { return querySingle(mdb.users.col, qPars) },
+  }
 }
 
-export default mdb
+export async function init () {
+  const db = await getDb(configs.mongodb.dbname);
+  const existingCols = (await db.collections()).map(x => x.collectionName);
+  for (const [colName, colInfo] of Object.entries(mdb)) {
+    colInfo.col = db.collection(colName);
+    if (!existingCols.includes(colName)) {
+      await db.createCollection(colName);
+    }
+    await colInfo._init();
+  }
+}
+
+export default mdb;
